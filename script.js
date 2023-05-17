2de25e31464440bc01fc23b3d0bb9d4a6c651803
Mateu-u <129677176+Mateu-u@users.noreply.github.com>
2023-05-17 20:01:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//Scroll to bottom
+const scrollButton = document.querySelector('.scroll-button');
+const Audio = document.getElementById('PlayAudio');
+
+scrollButton.addEventListener('click', function() {
+  document.body.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest'});
+  Audio.play();
+});
+
+//Scroll to top
+const topscrollButton = document.getElementById("scroll-to-top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
+    topscrollButton.style.display = "block";
+  } else {
+    topscrollButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+const Yes = document.querySelector('.autor');
+
+Yes.addEventListener('click', ()=> {
+  
+})
